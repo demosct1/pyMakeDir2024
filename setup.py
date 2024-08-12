9257462fc8ad55af36d589a680e929ea09ed0e4e
pifaces <thangaraj.r@solutionchamps.com>
2024-08-12 12:55:00 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Makedir',
+    version='0.1.0',
+    description='make Dir',
+    author='Thangaraj R',
+    author_email='sct.python.log@gmail.com',
+    packages=['torch'],
+)
